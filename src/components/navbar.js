0edc7f37d122807e9d6b981603e00601de437550
alexjamesmx <sg.alex32@gmail.com>
2024-07-28 08:22:29 -0600

    set base_url

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -111,7 +111,7 @@ export default function Navbar({ is_game }) {
                   </li>
                   <li className="content-center">
                     <a
-                      href="#"
+                      href="/about"
                       className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                     >
                       About
@@ -119,10 +119,10 @@ export default function Navbar({ is_game }) {
                   </li>
                   <li className="content-center">
                     <a
-                      href="#"
+                      href="/pricing"
                       className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                     >
-                      Services
+                      Pricing
                     </a>
                   </li>
                   <li>
